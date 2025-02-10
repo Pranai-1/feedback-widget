e39a00495017167ec447a640532e53f39c15dfff
Pranai-1 <reddypranai2017@gmail.com>
2025-02-10 15:59:57 +0530

    Removed node_modules and dist from tracking

--- dist/components/FeedbackWidget.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-declare function FeedbackWidget({ pageName, spaceName }: {
-    pageName: string;
-    spaceName: string;
-}): React.JSX.Element;
-export { FeedbackWidget };
